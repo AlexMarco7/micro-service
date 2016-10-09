bd69806fc764fcc5af7025e614aef16a4eee4f67
Alexandre <alexandre.dosmarcos@gmail.com>
2016-10-09 00:03:02 -0300

    first commit

--- dist/action/teste.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const action_1 = require('../action');
-class Teste extends action_1.Action {
-    process(data, headers, reply, fail) {
-        console.log("aaquiiii - " + JSON.stringify(data) + " - " + JSON.stringify(headers));
-        reply(data);
-    }
-}
-exports.Teste = Teste;
